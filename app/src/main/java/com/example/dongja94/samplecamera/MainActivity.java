58e4957f6b3a50ce9f362a57dd09c2b09d76f459
dongja94 <dongja94@gmail.com>
2016-03-14 09:58:01 +0900

    runtime permission

--- app/src/main/java/com/example/dongja94/samplecamera/MainActivity.java
+++ app/src/main/java/com/example/dongja94/samplecamera/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.dongja94.samplecamera;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -22,7 +27,36 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         surfaceView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 
+    private static final int RC_PERMISSION = 100;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openCamera();
+            startPreview();
+        }
+    }
+
     private void openCamera() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("permission");
+                builder.setMessage("camera permission....");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, RC_PERMISSION);
+                    }
+                });
+                builder.setCancelable(false);
+                builder.create().show();
+                return;
+            }
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, RC_PERMISSION);
+            return;
+        }
         if (mCamera != null) {
             releaseCamera();
         }
@@ -39,32 +73,44 @@ public class MainActivity extends AppCompatActivity implements SurfaceHolder.Cal
         mCamera = null;
     }
 
+    SurfaceHolder mHolder;
+
+    private void startPreview() {
+        if (mCamera != null && mHolder != null) {
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void stopPreview() {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+        }
+    }
+
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         openCamera();
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        mHolder = holder;
+        startPreview();
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        mCamera.stopPreview();
+        stopPreview();
 
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        mHolder = holder;
+        startPreview();
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        mCamera.stopPreview();
+        stopPreview();
+        mHolder = null;
         releaseCamera();
     }
 }
